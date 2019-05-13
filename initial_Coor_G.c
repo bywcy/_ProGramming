346873b5b8be9241a39dc9d817fa12decf3072a5
hycsy2019 <49811259+hycsy2019@users.noreply.github.com>
2019-05-13 21:21:59 +0800

    initial_Coor_G function.2019.5.13
    头文件不是最终版，先浏览一下有没有什么问题...
    刚刚输入输出函数发的时候忘加时间了sorry

---
+++ initial_Coor_G.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include "initialize.h"
+
+COOR_G CG[17][17];
+
+void initial_Coor_G(COOR_G a[][17])
+{
+	int i,j;
+	for(i=0;i<=16;i++)
+	{
+		for(j=0;j<=16;j++)
+		{
+			//initialize tag
+			if(i%2==1||j%2==1)
+			{
+				a[i][j].tag=0;//road
+				a[i][j].index=0;
+		    }
+			else //not road
+			{
+				a[i][j].tag=-1;
+				a[i][j].index=-1;//the index and the tag will be given values later in the assigning function 
+			}
+			
+			//intialize the coordinates of every index
+			a[i][j].x=i;
+			a[i][j].y=j;
+			
+			//intialize the arounding indexes'coordinates of every index
+			a[i][j].arou[1].x=i;a[i][j].arou[1].y=j-1;
+			if(i%2==1||(j-1)%2==1)
+			{
+				a[i][j].arou[1].tag=0;
+				a[i][j].arou[1].index=0;
+			}
+			else 
+			{
+				a[i][j].arou[1].tag=-1;
+				a[i][j].arou[1].index=-1;
+			}
+			
+			a[i][j].arou[2].x=i;a[i][j].arou[2].y=j+1;
+			if(i%2==1||(j+1)%2==1)
+			{
+				a[i][j].arou[2].tag=0;
+				a[i][j].arou[2].index=0;
+			}
+			else 
+			{
+				a[i][j].arou[2].tag=-1;
+				a[i][j].arou[2].index=-1;
+			}
+			
+			a[i][j].arou[3].x=i-1;a[i][j].arou[3].y=j;
+			if((i-1)%2==1||(j%2==1))
+			{
+				a[i][j].arou[3].tag=0;
+				a[i][j].arou[3].index=0;
+			}
+			else 
+			{
+				a[i][j].arou[3].tag=-1;
+				a[i][j].arou[3].index=-1;
+			}
+			
+			a[i][j].arou[4].x=i+1;a[i][j].arou[4].y=j;
+			if((i+1)%2==1||j%2==1)
+			{
+				a[i][j].arou[4].tag=0;
+				a[i][j].arou[4].index=0;
+			}
+			else 
+			{
+				a[i][j].arou[4].tag=-1;
+				a[i][j].arou[4].index=-1;
+			}
+		}
+	}
+}
+
+/*//You can use the following code to test the validity of the function above.
+void outputXY(COOR_G array[][17],int rows,int cols)
+{
+	int row,col;
+	for(row=0;row<=rows;row++)
+	{
+		for(col=0;col<=cols;col++)
+		{
+			printf("(%d,%d) ",array[row][col].x,array[row][col].y);
+			printf("up=(%d,%d) ",array[row][col].arou[1].x,array[row][col].arou[1].y);
+			printf("down=(%d,%d) ",array[row][col].arou[2].x,array[row][col].arou[2].y);
+			printf("left=(%d,%d) ",array[row][col].arou[3].x,array[row][col].arou[3].y);
+			printf("right=(%d,%d)\n",array[row][col].arou[4].x,array[row][col].arou[4].y);
+		}
+	}
+}
+
+void outputArray(COOR_G array[][17],int rows,int cols)
+{
+	int row,col;
+	for(row=0;row<rows;row++)
+	{
+		for(col=0;col<cols;col++)
+		{
+			if(col!=cols-1)
+			printf("(%d,%d)   ",array[row][col].tag,array[row][col].index);
+			else printf("(%d,%d)\n\n",array[row][col].tag,array[row][col].index);
+		}
+	}
+}
+
+ int main()
+ {
+ 	initial_Coor_G(CG);
+	outputXY(CG,16,16);
+	outputArray(CG,16,16);
+ }
+ 
+ //2019.5.12 11:31 a.m. by Cui Siying 
---
+++ initialize.h
@@ -0,0 +1,37 @@
+//initialize module 
+//including building graph module and order module
+
+#ifndef INITIALIZE_H
+#define INITIALIZE_H 
+
+typedef struct coor_G_index_around//store arounding indexes of every index in the coordinate graph
+{
+	int x,y;
+	int tag;
+	int index;
+}AROUND;
+
+typedef struct coor_G_index//information of the indexes of the coordinate graph
+{
+	int index;//the number of the restaurant or the client if tag = 1 or 2
+	int tag;//mark the index to judge whether it's road(0)/cli(1)/res(2)
+	int x,y;//coordinates
+	AROUND arou[5];//1:up.2:down.3:left.4:right
+}COOR_G;
+
+extern COOR_G CG[][17];
+
+void initial_Coor_G();//initial coordinate graph 
+void initial_Rel_G();//initial relational graph 
+void initial_Tree();//initial tree
+void initial_Ord_Q();//initial order queue
+
+//initialize inner module
+//these Functions should be only used in the Functions of initial module
+
+void confirm_Id();//confirm which node is road / cli / res
+int assign_Ind();//assign index to every road_nodes 
+void connect_Rel();//connect relaitionship between res / cli and road
+
+#endif 
+
